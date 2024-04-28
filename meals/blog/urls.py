08f294dce67541d72487e27c9cbc28c528c02790
Atib Pokharel <atibp1722@gmail.com>
2024-04-28 14:36:15 +0545

    search function start

--- meals/blog/urls.py
+++ meals/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('specific',views.specific,name='specific'),
+    path('saerch',views.search,name='search'),
 ]
--- meals/blog/views.py
+++ meals/blog/views.py
@@ -12,3 +12,13 @@ def index(request):
 
 def specific(request):
     return HttpResponse('Test 2')
+
+def search(request):
+    if request.method=='POST':
+        user_search=requests.POST.get('user_search')
+        response=requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+user_search+"&app_id=35339e85&app_key=1da925c3ec3e865c20c5fe6db3e3f8c5")
+        json_response=response.json()
+        meals=json_response['hits']
+        return render(request,'blog/index.html',{'meals':meals})
+    else:
+        return render(request,'blog/index.html')
