6b2700dbed25c34704512341a5683106827a661a
Atib Pokharel <atibp1722@gmail.com>
2024-04-28 13:52:02 +0545

    loop through api keys to get wanted info

--- meals/blog/views.py
+++ meals/blog/views.py
@@ -6,7 +6,9 @@ import requests
 def index(request):
     meal='chicken'
     response=requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+meal+"&app_id=35339e85&app_key=1da925c3ec3e865c20c5fe6db3e3f8c5")
-    return render(request,'blog/index.html')
+    json_response=response.json()
+    meals=json_response['hits']
+    return render(request,'blog/index.html',{'meals':meals})
 
 def specific(request):
     return HttpResponse('Test 2')
