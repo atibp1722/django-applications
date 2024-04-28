023e15c8b1dd8446c2d28b323b1e08d922b44c3f
Atib Pokharel <atibp1722@gmail.com>
2024-04-28 12:30:34 +0545

    more urls created for app

--- meals/blog/urls.py
+++ meals/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('specific',views.specific,name='specific'),
+    path('meal/<int:meal_id>',views.meal,name='meal')
 ]
