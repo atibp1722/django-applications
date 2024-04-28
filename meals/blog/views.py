10511221a7d1ca345c6e33d388f07b6304b66816
Atib Pokharel <atibp1722@gmail.com>
2024-04-28 13:32:53 +0545

    setup api and enhance ui for the app

--- meals/blog/views.py
+++ meals/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
+    meal='chicken'
+    response=requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+meal+"&app_id=35339e85&app_key=1da925c3ec3e865c20c5fe6db3e3f8c5")
     return render(request,'blog/index.html')
 
 def specific(request):
