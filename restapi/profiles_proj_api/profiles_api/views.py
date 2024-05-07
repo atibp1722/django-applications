8dea1f48ffc6affe07ef008f272db47c25501f2f
Atib Pokharel <atibp1722@gmail.com>
2024-05-07 15:13:29 +0545

    created permission to ensure anonymous modification of profile status cannot occur

--- restapi/profiles_proj_api/profiles_api/views.py
+++ restapi/profiles_proj_api/profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 from . import serializers
@@ -97,6 +98,7 @@ class ProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes=(TokenAuthentication,)
     serializer_class=(serializers.ProfileFeedSerializer,)
     queryset=models.ProfileFeed.objects.all()
+    permission_classes=(permissions.UpdateProfileStatus,IsAuthenticatedOrReadOnly)
     
     def perform_create(self,serializer):
         serializer.save(user_profile=self.request.user)
