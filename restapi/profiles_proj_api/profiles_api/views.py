9cbf7ff5d554268c770e0a17b8ce75575db3a9a8
Atib Pokharel <atibp1722@gmail.com>
2024-05-07 12:35:41 +0545

    created base api views, setup urls to test api view and finally test it

--- restapi/profiles_proj_api/profiles_api/views.py
+++ restapi/profiles_proj_api/profiles_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class BaseAPIView(APIView):
+
+    def get(self,request,format=None):
+        apiview=[
+            'HTTP methods to perform API related operations',
+            'Same as django view, it is epecially used for api',
+            'Mapped manually to urls'
+        ]
+        return Response({'message':apiview})
