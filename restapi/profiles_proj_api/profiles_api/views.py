a1ea9dba474a8795d587eccd89e897c3164fa0a6
Atib Pokharel <atibp1722@gmail.com>
2024-05-07 13:57:49 +0545

    initial commit for profile api

--- restapi/profiles_proj_api/profiles_api/views.py
+++ restapi/profiles_proj_api/profiles_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 
-from profiles_api import serializers
+from . import serializers
 
 
 class BaseAPIView(APIView):
