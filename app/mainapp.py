57ab81c82bca12febd6c36c991180364e41bf62b
Atib Pokharel <atibp1722@gmail.com>
2024-05-09 13:34:11 +0545

    get neccessary inforamtion from the api, create area to display weather information

--- app/mainapp.py
+++ app/mainapp.py
@@ -4,8 +4,9 @@ from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivymd.uix.list import MDList
-from kivymd.uix.list import OneLineListItem
+from kivymd.uix.list import OneLineListItem,ThreeLineAvatarListItem
 from kivymd.theming import ThemableBehavior
+from kivy.properties import StringProperty
 import requests
 
 
@@ -21,6 +22,10 @@ class AboutPage(Screen):
     pass
 
 
+class WeatherList(ThreeLineAvatarListItem):
+    source=StringProperty('string')
+
+
 class MainWidget(BoxLayout):
     pass
 
@@ -31,9 +36,19 @@ class MainApp(MDApp):
         return MainWidget()
     
     def on_start(self):
-        value=12,32,15,37,17
+        value='12,32,15,37,17'
         hlist=self.root.ids.home_list
         response=requests.get('https://api.openweathermap.org/data/2.5/box/city?bbox='+value+'&appid=ecd2457b5bc4ae41ec0b12d3cf7f300f')
-    
+        output=response.json()
+        response_list=output['list']
+
+        for i in response_list:
+            name=i['name']
+            temp=str(i['main']['temp'])
+            temp_max=str(i['main']['temp_max'])
+            temp_min=str(i['main']['temp_min'])
+            i=WeatherList(text=name,secondary_text=temp,tertiary_text=temp_max)
+            hlist.add_widget(i)
+        
 
 MainApp().run()  
