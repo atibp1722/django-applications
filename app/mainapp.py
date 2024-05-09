822a3df749e73e4370c223c66d49c2a26deacf09
Atib Pokharel <atibp1722@gmail.com>
2024-05-09 12:36:58 +0545

    added about page

--- app/mainapp.py
+++ app/mainapp.py
@@ -1,11 +1,12 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen,ScreenManager
+from kivymd.uix.list import MDList
+from kivymd.uix.list import OneLineListItem
+from kivymd.theming import ThemableBehavior
 
-
-class MainWidget(BoxLayout):
-    pass
 
 
 class ContentNavigationDrawer(BoxLayout):
@@ -13,6 +14,14 @@ class ContentNavigationDrawer(BoxLayout):
 
 
 class HomePage(Screen):
+    pass
+
+
+class AboutPage(Screen):
+    pass
+
+
+class MainWidget(BoxLayout):
     pass
 
 
