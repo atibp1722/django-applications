788cbdd3d53de3029ba34cd357d28e1b5b1ca866
Atib Pokharel <atibp1722@gmail.com>
2024-05-09 12:12:00 +0545

    navigation area created in kivy and and corresponding classes also added to the main class

--- app/app.py
+++ app/app.py
@@ -1,9 +1,18 @@
 from kivy.app import App
 from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen,ScreenManager
 
 
 class MainWidget(BoxLayout):
+    pass
+
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class HomePage(Screen):
     pass
 
 
