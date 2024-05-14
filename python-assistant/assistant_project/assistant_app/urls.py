aad52c1a2d85c2d8e4ff9197d79fd2b2181842e8
Atib Pokharel <atibp1722@gmail.com>
2024-05-14 17:20:23 +0545

    added view to the get the news in json format, created js function to get the news from the api service and display in the screen

--- python-assistant/assistant_project/assistant_app/urls.py
+++ python-assistant/assistant_project/assistant_app/urls.py
@@ -6,9 +6,7 @@ urlpatterns=[
     # RETURN DEFAULT PAGE
     path('',views.index,name='index'),
 
-    # TEST FOR SPECIFICALLY NAMED URL
-    # path('named_url/',views.named,name='named_url'),
-
     path('getResponse',views.getResponse,name='getResponse'),
     path('getWeather',views.getWeather,name='getWeather'),
+    path('getNews',views.getNews,name='getNews'),
 ]
--- python-assistant/assistant_project/assistant_app/views.py
+++ python-assistant/assistant_project/assistant_app/views.py
@@ -7,10 +7,6 @@ def index(request):
     return render(request,'assistant_app/index.html')
 
 
-# def named(request):
-#     return HttpResponse('Named URL')
-
-
 def getResponse(request):
     msg=request.GET.get('userMessage')
     return HttpResponse(msg)
@@ -19,9 +15,15 @@ def getResponse(request):
 def getWeather(request):
     lat_=str(request.GET.get('lat'))
     long_=str(request.GET.get('long'))
-    reqs=requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat_+"&lon="+long_+"&appid=ecd2457b5bc4ae41ec0b12d3cf7f300f")
+    reqs=requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat_+"&lon="+long_+"&units=metric&appid=ecd2457b5bc4ae41ec0b12d3cf7f300f")
     json_response=reqs.json()
     name=json_response['name']
     temp=json_response['main']['temp']
     desc=json_response['weather'][0]['description']
+    return JsonResponse(json_response)
+
+
+def getNews(request):
+    reqs=requests.get("https://saurav.tech/NewsAPI/everything/cnn.json")
+    json_response=reqs.json()
     return JsonResponse(json_response)
